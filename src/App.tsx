812f0d1f8bfcf58e807c123f20e8b5436676d6e0
etrudel <etrudel@planifika.net>
2022-04-16 09:19:47 -0400

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import logo from "./bowling.jpeg";
 import "./App.css";
-import GameScreen from "./view/GameScreen";
-import { Routes, Route, Link } from "react-router-dom";
-import Button from "@mui/material/Button";
 import StartPage from "./view/startPage";
 
 function App() {
