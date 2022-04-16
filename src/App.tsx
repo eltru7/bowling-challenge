24f7ba747e123beab390699a06cf1b4fc342e823
etrudel <etrudel@planifika.net>
2022-04-16 09:19:06 -0400

    create start page

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,12 @@ import "./App.css";
 import GameScreen from "./view/GameScreen";
 import { Routes, Route, Link } from "react-router-dom";
 import Button from "@mui/material/Button";
+import StartPage from "./view/startPage";
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <nav>
-        <Button variant="outlined">
-          <Link to="/game">Start a new game</Link>
-        </Button>
-      </nav>
-      <Routes>
-        <Route path="game" element={<GameScreen />} />
-      </Routes>
+      <StartPage />
     </div>
   );
 }
