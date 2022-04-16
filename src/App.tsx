286c46806367b5cb19135d9d054e5a5f5fa9df43
etrudel <etrudel@planifika.net>
2022-04-16 09:12:01 -0400

    replace header by adding logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./bowling.jpeg";
 import "./App.css";
 import GameScreen from "./view/GameScreen";
 import { Routes, Route, Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 function App() {
-  const startNewGame = (): void => {
-    console.log("start new game");
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
       <nav>
-        <Link to="/game">Start a new game</Link>
+        <Button variant="outlined">
+          <Link to="/game">Start a new game</Link>
+        </Button>
       </nav>
-
       <Routes>
         <Route path="game" element={<GameScreen />} />
       </Routes>
